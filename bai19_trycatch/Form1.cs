17a830f19245f176168fce3c85faf11688676684
SPC <SPC@05-LAB402>
2025-04-11 08:00:37 +0700

    Add project files.

---
+++ bai19_trycatch/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bai19_trycatch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int so = Convert.ToInt32(textBox1.Text);
+                label1.Text = (100 + so).ToString();
+            }
+            catch {
+                label1.Text = "vui lòng nhập số nguyên";
+            }
+            
+           
+        }
+    }
+}
